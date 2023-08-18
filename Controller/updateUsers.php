877ed2c5683c39bc93c1a840cae74e653975f93d
VolodymyrNehir <volodymyrnehir@gmail.com>
2023-08-19 01:58:45 +0300

    add

--- Controller/updateUsers.php
+++ Controller/updateUsers.php
@@ -1,13 +1,31 @@
 <?php
 include_once "../Model/Model.php";
-include_once "../codeStatus/CodeStatus.php";
+//include_once "../codeStatus/CodeStatus.php";
 
-$form =$_POST['form'];
+$form = $_POST['form'];
 
 
 $pdo = new Model();
-foreach ($form['userId'] as $user){
-    $res =  $pdo->setAction($user,$form['select']);
-   $arrStatus[] = CodeStatus::myStatusCodeUsers($user,$res,'100','failed to provide status');
+
+foreach ($form['userId'] as $userId) {
+    $user = $pdo->getById($userId);
+    $errorStatus = $pdo->setAction($userId, $form['select']);
+    if ($errorStatus == false) {
+        $response[] = ["status" => $errorStatus, "error" => ["code" => '100', "user" => $user], "user" =>
+            [
+                "id" => $user,
+                "firstName" => $user['firstName'],
+                "lastName" => $user['lastName'],
+                "role" => $user['role'],
+                "status" => $user['status'],
+            ]
+        ];
+    }
+    else {
+        $response[] = ["status" => $errorStatus, "error" => ["code" => '100', "message" => 'failed to provide status']];
+
+    }
+//    $res = $pdo->setAction($user, $form['select']);
+//    $arrStatus[] = CodeStatus::myStatusCodeUsers($user, $res, '100', 'failed to provide status');
 }
-echo json_encode($arrStatus);
+echo json_encode($response);
