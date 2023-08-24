7e0784a652d1d3afaf2143881ba9cd4833f0a3d2
VolodymyrNehir <volodymyrnehir@gmail.com>
2023-08-25 00:50:42 +0300

    update Model

--- Model/Model.php
+++ Model/Model.php
@@ -3,25 +3,27 @@
 class Model
 {
     private $pdo;
+    const HOST = 'localhost:8889';
+    const DB = 'users';
+    const USER = 'root';
+    const PASSWORD = 'root';
+    const TABLE = 'users';
 
     public function __construct()
     {
-        $host = 'localhost:8889';
-        $db = 'users';
-        $user = 'root';
-        $password = 'root';
-        try {
-            $dsn = "mysql:host=$host;dbname=$db";
-            $this->pdo = new PDO($dsn, $user, $password);
-        } catch (PDOException $e) {
-            echo "error: " . $e->getMessage();
-        }
+            try {
+                $dsn = "mysql:host=" . self::HOST . ";dbname=" . self::DB;
+                $this->pdo = new PDO($dsn, self::USER, self::PASSWORD);
+            } catch (PDOException $e) {
+                echo "error: " . $e->getMessage();
+            }
 
     }
 
     public function getById($id)
     {
-        $query = $this->pdo->query("SELECT * FROM `users` WHERE `users`.`id` = $id");
+        $query = $this->pdo->prepare("SELECT * FROM `" . self::TABLE . "` WHERE `" . self::TABLE . "`.`id` = ?");
+        $query->execute([$id]);
         $arr = $query->fetchAll(PDO::FETCH_ASSOC);
         foreach ($arr as $user ){
             return $user;
@@ -31,36 +33,36 @@ class Model
 
     public function addUser($lastName, $firstName, $role, $status)
     {
-        $sql = "INSERT INTO `users` (`id`, `lastName`, `firstName`, `role`, `status`) VALUES (NULL, ?, ?, ?, ?)";
+        $sql = "INSERT INTO `" . self::TABLE . "` (`id`, `lastName`, `firstName`, `role`, `status`) VALUES (NULL, ?, ?, ?, ?)";
         $prepare = $this->pdo->prepare($sql);
         $prepare->execute([$lastName, $firstName, $role, $status]);
-        return $this->pdo->lastInsertId();    }
+        return $this->pdo->lastInsertId();
+    }
 
     public function deleteUsers($id)
     {
-        $sql = "DELETE FROM `users` WHERE `users`.`id` = $id";
+        $sql = "DELETE FROM `" . self::TABLE . "` WHERE `" . self::TABLE . "`.`id` = ?";
         $prepare = $this->pdo->prepare($sql);
-        return $prepare->execute();
+        return $prepare->execute([$id]);
     }
 
     public function upUsers($id, $lastName, $firstName, $role, $status)
     {
-        $sql = "UPDATE `users` SET `lastName` = ?, `firstName` = ?, `role` = ?, `status` = ? WHERE `users`.`id` = $id";
+        $sql = "UPDATE `" . self::TABLE . "` SET `lastName` = ?, `firstName` = ?, `role` = ?, `status` = ? WHERE `" . self::TABLE . "`.`id` = ?";
         $prepare = $this->pdo->prepare($sql);
-        $prepare->execute([$lastName, $firstName, $role, $status]);
+        $prepare->execute([$lastName, $firstName, $role, $status, $id]);
     }
 
     public function setAction($id, $status)
     {
-
-        $sql = "UPDATE `users` SET `status` = ? WHERE `users`.`id`=$id";
+        $sql = "UPDATE `" . self::TABLE . "` SET `status` = ? WHERE `" . self::TABLE . "`.`id` = ?";
         $prepare = $this->pdo->prepare($sql);
-        $prepare->execute([$status]);
+        $prepare->execute([$status, $id]);
     }
 
     public function selectUsers()
     {
-        $query = $this->pdo->query('SELECT * FROM `users`');
+        $query = $this->pdo->query('SELECT * FROM `' . self::TABLE . '`');
         return $query->fetchAll(PDO::FETCH_ASSOC);
     }
 }
